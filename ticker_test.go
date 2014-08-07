93589fcaa54388e5e06552628c49871debaf8217
Rajiv Kilaparti <rajivk085@gmail.com>
2014-08-07 23:29:23 +0530

    add test files
     * add test code

---
+++ ticker_test.go
@@ -0,0 +1,18 @@
+package ticker
+
+import "testing"
+import "fmt"
+
+func print(args []interface{}) {
+	fmt.Printf("Hello World !! Got %d args\n", len(args))
+	for i, s := range args {
+		fmt.Println(i, s)
+	}
+}
+
+func ExampleTickEvery() {
+	stop := ticker.TickEvery(print, time.Second*3, []interface{}{"a", 2})
+	time.Sleep(time.Second * 10) // sleep for 10 seconds, to see 3 print invocations
+	stop <- true                 // send a stop signal
+	time.Sleep(time.Second * 2)  // sleep for 2 more seconds, to ensure, it stopped :)
+}
